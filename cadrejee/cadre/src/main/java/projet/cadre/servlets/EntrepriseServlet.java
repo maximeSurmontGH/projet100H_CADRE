91078aa828611bd50c2431562af4db59c8c584d6
Chloe <chloe.pelletier@hei.fr>
2017-02-12 16:31:58 +0100

    jee site client ajouté

---
+++ cadrejee/cadre/src/main/java/projet/cadre/servlets/EntrepriseServlet.java
@@ -0,0 +1,37 @@
+package projet.cadre.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@WebServlet("/entreprise")
+
+public class EntrepriseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(req.getServletContext());
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.addDialect(new Java8TimeDialect());	
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+
+		templateEngine.process("page-lentreprise", context, resp.getWriter());
+	
+
+}
+}
