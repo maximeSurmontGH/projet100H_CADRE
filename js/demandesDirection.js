8111f61dce7c900fe8333f74fef6913907d79a68
maximeS <maxime.surmont@hei.fr>
2017-02-14 00:07:36 +0100

    commit de la page demandesDirection v2

--- js/demandesDirection.js
+++ js/demandesDirection.js
@@ -91,8 +91,12 @@ function gestionnairePage2(pageModifiee){
 		th5.innerHTML = "Décision";
 		tr.appendChild(th5);
 		table.appendChild(tr);
+
+		createurDeLigneVC("SURMONT Maxime", "Camionnette 2", "13/05/17", "15/05/17", "actuel", "1");
+		createurDeLigneVC("SURMONT Maxime", "Camionnette 2", "13/05/17", "15/05/17", "refus", "2");
+		createurDeLigneVC("SURMONT Maxime", "Camionnette 2", "13/05/17", "15/05/17", "succes", "3");
 	}
-	
+
 	if(pageModifiee=="conges"){
 		document.getElementById("sousTitre1").innerHTML = "Demandes en cours : ";
 		document.getElementById("sousTitre1").className = "sectionP";
@@ -137,6 +141,10 @@ function gestionnairePage2(pageModifiee){
 		th5.innerHTML = "Décision";
 		tr.appendChild(th5);
 		table.appendChild(tr);
+
+		createurDeLigneVC("SURMONT Maxime", "Congé maternité", "13/05/17", "15/05/17", "actuel", "1");
+		createurDeLigneVC("SURMONT Maxime", "Congé payé", "13/05/17", "15/05/17", "refus", "2");
+		createurDeLigneVC("SURMONT Maxime", "Congé payé", "13/05/17", "15/05/17", "succes", "3");
 	}
 
 	if(pageModifiee=="attestations"){
@@ -177,6 +185,10 @@ function gestionnairePage2(pageModifiee){
 		th4.innerHTML = "Décision";
 		tr.appendChild(th4);
 		table.appendChild(tr);
+
+		createurDeLigneA("SURMONT Maxime", "Attestation1", "", "actuel", "1");
+		createurDeLigneA("SURMONT Maxime", "Attestation1", "", "refus", "2");
+		createurDeLigneA("SURMONT Maxime", "Attestation2", "15/05/17", "succes", "3");
 	}
 
 	if(pageModifiee=="vehicules2"){
@@ -226,41 +238,171 @@ function gestionnairePage2(pageModifiee){
 	}
 }
 
-function createurDeLigne (element, action, num){
-	var table = document.getElementById("tableauDesModifications1");
+function createurDeLigneVC(nom, type, dateD, dateF, genre, num){
+	if (genre=="actuel"){
+		var table = document.getElementById("tableauDesDemandes1");
+
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var a1 = document.createElement('a');
+		a1.href="#";
+		a1.id="a1"+num;
+		var span1 = document.createElement('span');
+		span1.className=" glyphicon glyphicon-ok spanSpe";
+		a1.appendChild(span1);
+		td5.appendChild(a1);
+		var a2 = document.createElement('a');
+		a2.href="#";
+		a2.id="a2"+num;
+		var span2 = document.createElement('span');
+		span2.className=" glyphicon glyphicon-remove spanSpe";
+		a2.appendChild(span2);
+		td5.appendChild(a2);
+	}
+
+	if (genre=="refus"){
+		var table = document.getElementById("tableauDesDemandes2");
+
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var span = document.createElement('span');
+		span.id="span"+num;
+		span.className="glyphicon glyphicon-remove";
+		td5.appendChild(span);
+	}
+
+	if (genre=="succes"){
+		var table = document.getElementById("tableauDesDemandes2");
+
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var span = document.createElement('span');
+		span.id="span"+num;
+		span.className="glyphicon glyphicon-ok";
+		td5.appendChild(span);
+	}
+
 	var tr = document.createElement('tr');
 	var td1 = document.createElement('td');
-	td1.innerHTML = element;
-	tr.appendChild(td1);
+	td1.innerHTML = nom;
 	var td2 = document.createElement('td');
-	td2.className="colonneLarge";
-	if (action=="message"){
-		var textarea = document.createElement('textarea');
-		textarea.className="inputTextarea form-control";
-		textarea.id="input"+num;
-		td2.appendChild(textarea);
-	}
-	else if (action=="photo"){
-		var input = document.createElement('input');
-		input.type="file";
-		input.id="input"+num;
-		td2.appendChild(input);
-	}
-	tr.appendChild(td2);
+	td2.innerHTML = type;
 	var td3 = document.createElement('td');
-	td3.className="colonneFine";
-	var a = document.createElement('a');
-	a.href="#";
-	a.id="a"+num;
-	var span = document.createElement('span');
-	span.className=" glyphicon glyphicon-new-window";
-	a.appendChild(span);
-	td3.appendChild(a);
+	td3.innerHTML = dateD;
+	var td4 = document.createElement('td');
+	td4.innerHTML = dateF;
+	tr.appendChild(td1);
+	tr.appendChild(td2);
 	tr.appendChild(td3);
+	tr.appendChild(td4);
+	tr.appendChild(td5);
 	table.appendChild(tr);
 }
 
-//fct verification jpg png
+function createurDeLigneA(nom, type, date, genre, num){
+	if (genre=="actuel"){
+		var table = document.getElementById("tableauDesDemandes1");
+
+		var td3 = document.createElement('td');
+		var sel1_1 = document.createElement('select');
+		sel1_1.id = "Jours";
+		var sel2_1 = document.createElement('select');
+		sel2_1.id = "Mois";
+		var sel3_1 = document.createElement('select');
+		sel3_1.id = "Annees";
+		td3.appendChild(sel1_1);
+		td3.appendChild(sel2_1);
+		td3.appendChild(sel3_1);
+
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var a1 = document.createElement('a');
+		a1.href="#";
+		a1.id="a1"+num;
+		var span1 = document.createElement('span');
+		span1.className=" glyphicon glyphicon-ok spanSpe";
+		a1.appendChild(span1);
+		td5.appendChild(a1);
+		var a2 = document.createElement('a');
+		a2.href="#";
+		a2.id="a2"+num;
+		var span2 = document.createElement('span');
+		span2.className=" glyphicon glyphicon-remove spanSpe";
+		a2.appendChild(span2);
+		td5.appendChild(a2);
+	}
+
+	if (genre=="refus"){
+		var table = document.getElementById("tableauDesDemandes2");
+
+		var td3 = document.createElement('td');
+		td3.innerHTML = date;
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var span = document.createElement('span');
+		span.id="span"+num;
+		span.className="glyphicon glyphicon-remove";
+		td5.appendChild(span);
+	}
+
+	if (genre=="succes"){
+		var table = document.getElementById("tableauDesDemandes2");
+
+		var td3 = document.createElement('td');
+		td3.innerHTML = date;
+		var td5 = document.createElement('td');
+		td5.className="colonneFine";
+		var span = document.createElement('span');
+		span.id="span"+num;
+		span.className="glyphicon glyphicon-ok";
+		td5.appendChild(span);
+	}
+
+	var tr = document.createElement('tr');
+	var td1 = document.createElement('td');
+	td1.innerHTML = nom;
+	var td2 = document.createElement('td');
+	td2.innerHTML = type;
+
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tr.appendChild(td3);
+	tr.appendChild(td5);
+	table.appendChild(tr);
+	remplissageJMA();
+}
+
+function remplissageJMA(){
+	var sel1_1 = document.getElementById('Jours');
+	for(var i = 1; i<32; i++){
+		var J = document.createElement('option');
+		J.innerHTML=i;
+		sel1_1.appendChild(J);
+	}
+	var sel2_1 = document.getElementById('Mois');
+	var M = document.createElement('option'); M.innerHTML="janvier"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="février"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="mars"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="avril"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="mai"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="juin"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="juillet"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="aout"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="septembre"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="octobre"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="novembre"; sel2_1.appendChild(M);
+	var M = document.createElement('option'); M.innerHTML="decembre"; sel2_1.appendChild(M);
+	var sel3_1 = document.getElementById('Annees');
+	for(var i = 2030; i>2000; i--){
+		var A = document.createElement('option');
+		A.innerHTML=i;
+		sel3_1.appendChild(A);
+	}
+}
+
+
+//fct accept refus passer ds l autre tableau
+//faire des pages de tableau
 
 window.onload = function(){
 	maillingAnnonce();
