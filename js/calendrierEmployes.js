580adbf3b0217b091fb58f18816cb1544cff22b4
maximeS <maxime.surmont@hei.fr>
2017-02-18 15:08:20 +0100

    Modification du JS réduction de page et affectation aux autres pages

--- js/calendrierEmployes.js
+++ js/calendrierEmployes.js
@@ -1,3 +1,48 @@
+function disparaitre(){
+	if (window.matchMedia("(max-width: 650px)").matches){
+		document.getElementById("navigation").className="disparaitreAlerte";
+		document.getElementById("iconePasla").id="iconeMenu";
+		document.getElementById("section").className="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1";
+	}
+	window.onresize=function(){
+  	if (window.matchMedia("(max-width: 650px)").matches){
+			document.getElementById("navigation").parentNode.className="disparaitreAlerte";
+    	document.getElementById("navigation").className="disparaitreAlerte";
+			document.getElementById("iconePasla").id="iconeMenu";
+			document.getElementById("section").className="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1";
+			ouvrirPetitMenu();
+  	}
+		else{
+			document.getElementById("navigation").style.left=0;
+			document.getElementById("navigation").className="col-lg-3 col-md-3 col-sm-3 col-xs-3";
+			document.getElementById("section").className="col-lg-8 col-md-8 col-sm-8 col-xs-8";
+			document.getElementById("iconeMenu").id="iconePasla";
+			document.getElementById("navigation").parentNode.className="";
+		}
+	}
+}
+
+function ouvrirPetitMenu (){
+	document.getElementById("iconeMenu").onclick=function(){
+		var width = window.innerWidth;
+		if (width>500){
+			var marginLeft = window.innerWidth*40/100;
+		}
+		else if (width>350){
+			var marginLeft = window.innerWidth*30/100;
+		}
+		else if (width>200){
+			var marginLeft = window.innerWidth*20/100;
+		}
+		document.getElementById("navigation").className="petitMenu";
+		document.getElementById("navigation").style.left=marginLeft+"px";
+		document.getElementById("navigation").parentNode.className="opaque";
+	}
+	document.getElementById("navigation").parentNode.onclick=function(){
+		document.getElementById("navigation").parentNode.className="disparaitreAlerte";
+	}
+}
+
 function maillingAnnonce(){
 	document.getElementById("mailling").onclick=function(){
 		var p = document.getElementById("maillingAnnonceNum");
@@ -345,4 +390,6 @@ window.onload = function(){
 	ajoutInfo(25, "fine", "Acceptation du prêt du véhicule HG-379", "");
 	ajoutInfo(26, "fine", "Acceptation du prêt du véhicule HG-379", "");
 	compteurInfos();
+	disparaitre();
+	ouvrirPetitMenu ();
 };
