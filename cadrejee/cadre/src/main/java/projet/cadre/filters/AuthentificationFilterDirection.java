5a66b43f26173bd08a77d9c64101d0c9fec9d963
Chloe <chloe.pelletier@hei.fr>
2017-02-21 23:27:51 +0100

    Initialisation de la page Direction dans JEE + connection intranet direction OK

---
+++ cadrejee/cadre/src/main/java/projet/cadre/filters/AuthentificationFilterDirection.java
@@ -0,0 +1,41 @@
+package projet.cadre.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthentificationFilterDirection implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String identifiant = (String) httpRequest.getSession().getAttribute("utilisateurConnecte");
+		if (identifiant == null || "".equals(identifiant)) {
+			System.out.println("Il faut être connecté pour accéder à cette page !");
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			httpResponse.sendRedirect("connexionIntranet");
+			return;
+		}
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+
+
+}
---
+++ cadrejee/cadre/src/main/java/projet/cadre/servlets/ConnexionServlet.java
@@ -0,0 +1,62 @@
+package projet.cadre.servlets;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import projet.cadre.util.MotDePasseUtils;
+
+
+@WebServlet("/connexionIntranet")
+public class ConnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Map<String,String> utilisateursAutorises;
+	
+	@Override
+	public void init() throws ServletException {
+		utilisateursAutorises=new HashMap<>();
+		utilisateursAutorises.put("direction", "af04f0ed7d34f427c70d6b8bfd2c08a6d2c3e36b6fc80298:a043e4bed45f1ee900f6b60f163f843ed746ebbea4ed286b");
+				
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String identifiantConnecte = (String) request.getSession().getAttribute("utilisateurConnecte");
+		if (identifiantConnecte == null || "".equals(identifiantConnecte)) {
+			RequestDispatcher view = request.getRequestDispatcher("WEB-INF/templates/connexionIntranet.html");
+			view.forward(request, response);
+			
+		} else {
+			response.sendRedirect("menuIntranetDirection");
+		}
+	
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String identifiantSaisi = request.getParameter("inputUser");
+		String motDePasseSaisi = request.getParameter("inputPassword");
+		try {
+			if(utilisateursAutorises.containsKey(identifiantSaisi) && MotDePasseUtils.validerMotDePasse(motDePasseSaisi, utilisateursAutorises.get(identifiantSaisi)))
+			{
+				request.getSession().setAttribute("utilisateurConnecte",identifiantSaisi);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			response.sendRedirect("connexionIntranet");
+	}
+
+}
