a73531f153359e13540e776460098570c28ced6f
chloepelletier <chloe.pelletier@hei.fr>
2017-04-10 21:35:29 +0200

    Create EvenementAutreDao.java

---
+++ cadrejee/cadre/src/main/java/projet/cadre/dao/EvenementAutreDao.java
@@ -0,0 +1,72 @@
+package projet.cadre.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import projet.cadre.model.EvenementAutre;
+
+
+public class EvenementAutreDao {
+	
+public EvenementAutre saveEvenementAutre(EvenementAutre event) {
+		
+		try {
+			Connection connection = DataSourceProvider.getDataSource().getConnection();
+			
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO `evenementsautres`(`idEvenement`,`dateEvenement`,`messageEvenement`,`employes_idEmploye`) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1,event.getIdEvenement());
+			stmt.setString(2,event.getDateEvenement());
+			stmt.setString(3,event.getMessageEvenement());
+			stmt.setString(4,event.getEmployes_idEmploye());
+			stmt.executeUpdate();
+			stmt.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return event;
+	
+	}
+
+
+// Pour supprimer un evenement
+
+public void deleteevenement(EvenementAutre evenement){
+	try {
+		Connection connection = DataSourceProvider.getDataSource().getConnection();
+		
+		PreparedStatement stmt = connection.prepareStatement("DELETE FROM evenementsautres WHERE idEvenement=?");
+		stmt.setInt(1,evenement.getIdEvenement());
+		stmt.executeUpdate();
+		stmt.close();
+		connection.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+// Récupérer un evenement grace à son id 
+public EvenementAutre getevenementById(int idevenement ) {
+	EvenementAutre evenement=null;
+	try (
+		Connection connection = DataSourceProvider.getDataSource().getConnection();
+		PreparedStatement stmt = connection.prepareStatement("SELECT * FROM evenementsautres WHERE idEvenement = ?")){
+			stmt.setInt(1, idevenement);
+			ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			 evenement = new EvenementAutre (rs.getInt("idEvenement"), rs.getString("dateEvenement"), rs.getString("messageEvenement"), rs.getString("employes_idEmploye"));
+		}
+		rs.close();
+		stmt.close();
+		connection.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return evenement;
+}
+
+	
+}
