a7911eac3104ef894662a1604a7b389224685cfd
chloepelletier <chloe.pelletier@hei.fr>
2017-04-10 21:28:34 +0200

    Update RappelDaoTestCase.java

--- cadrejee/cadre/src/test/java/projet/cadre/dao/RappelDaoTestCase.java
+++ cadrejee/cadre/src/test/java/projet/cadre/dao/RappelDaoTestCase.java
@@ -12,6 +12,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import projet.cadre.model.Employe;
 import projet.cadre.model.Rappel;
 import projet.cadre.model.Ressource;
 
@@ -23,23 +24,25 @@ public class RappelDaoTestCase {
 		try (Connection connection = DataSourceProvider.getDataSource().getConnection();
 			Statement stmt = connection.createStatement()) {
 			stmt.executeUpdate("DELETE FROM rappels");
+			stmt.executeUpdate("DELETE FROM evenementsautres");
 			stmt.executeUpdate("DELETE FROM employes");
 			stmt.executeUpdate("INSERT INTO `employes`(`idEmploye`,`nomEmploye`,`prenomEmploye`,`motDePasse`,`poste`,`telephone`,`email`) VALUES ('chloe.pelletier','Pelletier', 'Chloe', '93292b27203e307bb1a6076042356e366517612a0f699b68:dc1ff13dc770dc5e2b176dff75a76dfce3c9744b3941138a','stagiaire','0614348499','chlo@g.com')");
 			stmt.executeUpdate("INSERT INTO `rappels`(`idRappel`,`dateRappel`,`messageRappel`,`employes_idEmploye`) VALUES (1,'2017-01-23','blabla','chloe.pelletier')");
 			stmt.executeUpdate("INSERT INTO `rappels`(`idRappel`,`dateRappel`,`messageRappel`,`employes_idEmploye`) VALUES (2,'2017-01-04','blabla3','chloe.pelletier')");
+			
 		}
 	}
 	
 	@Test
 	public void shouldSaveRappel() throws Exception {
 		// GIVEN
-		Rappel rappeltoSave = new Rappel("10122019","blablatest","chloe.pelletier");
-		// WHEN
-		Rappel rappelAdded = rappelDao.saveRappel(rappeltoSave) ;
+				Rappel rappeltoSave = new Rappel(3,"10122019","blablatest","chloe.pelletier");
+				// WHEN
+				Rappel rappelAdded = rappelDao.saveRappel(rappeltoSave) ;
 		// THEN
 		Assertions.assertThat(rappelAdded).isNotNull();		
 		try (Connection connection = DataSourceProvider.getDataSource().getConnection();
-			PreparedStatement stmt = connection.prepareStatement("SELECT * FROM rappels WHERE messageRappel = ?")) {
+				PreparedStatement stmt = connection.prepareStatement("SELECT * FROM rappels WHERE messageRappel = ?")) {
 			stmt.setString(1,"blablatest");
 			try (ResultSet rs = stmt.executeQuery()) {
 				assertThat(rs.next()).isTrue();
@@ -47,4 +50,36 @@ public class RappelDaoTestCase {
 			}
 		}
 	}
+
+	@Test
+	public void shouldDeleteRappel() throws Exception {
+		Rappel rappel= rappelDao.getRappelById(1);
+		// WHEN
+		rappelDao.deleteRappel(rappel);
+		// THEN
+		try (
+			Connection connection = DataSourceProvider.getDataSource().getConnection();
+			PreparedStatement stmt = connection.prepareStatement("SELECT * FROM rappels WHERE idRappel = ?")) {
+			stmt.setInt(1, 1);
+			try (ResultSet rs = stmt.executeQuery()) {
+				assertThat(rs.next()).isFalse();
+				
+			}
+		}
+		
+	}
+	
+	@Test
+	public void shouldGetRappelById() {
+		// WHEN
+		Rappel rappel = rappelDao.getRappelById(2);
+		// THEN
+		Assertions.assertThat(rappel).isNotNull();
+		Assertions.assertThat(rappel.getIdRappel()).isEqualTo(2);
+		Assertions.assertThat(rappel.getEmployes_idEmploye()).isEqualTo("chloe.pelletier");
+	}
+	
+	
+
+
 }
