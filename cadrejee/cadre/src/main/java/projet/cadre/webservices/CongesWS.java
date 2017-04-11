076ea433b8552146f4fb3117534242e59ce1de05
chloepelletier <chloe.pelletier@hei.fr>
2017-04-11 22:15:15 +0200

    Implementation conges webservice

--- cadrejee/cadre/src/main/java/projet/cadre/webservices/CongesWS.java
+++ cadrejee/cadre/src/main/java/projet/cadre/webservices/CongesWS.java
@@ -1,45 +1,61 @@
 package projet.cadre.webservices;
 
-import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-@Path("")
+import projet.cadre.services.CadreServices;
+
+@Path("/conges")
 public class CongesWS {
 	
 	final GsonBuilder builder = new GsonBuilder();
 	final Gson gson = builder.create();
 	
 	@GET
-	@Path("")
-	public Response listPokemon(){
-		return Response.status(200).entity(gson.toJson("")).build();
+	@Path("/listConges")
+	public Response listConges(){
+		CadreServices cadreservices = CadreServices.getInstance();
+		System.out.println(cadreservices.getDemandesDeConge());
+		return Response.status(200).entity(gson.toJson(cadreservices.getDemandesDeConge())).build();
 	}
 	
+	@GET
+	@Path("/compteurConges/{idConge}/{idEmploye}")
+	public Response compteurConges(@PathParam("idConge") int idConge,@PathParam("idEmploye") String idEmploye){
+		CadreServices cadreservices = CadreServices.getInstance();
+		System.out.println(cadreservices.compteurDeConges(idConge, idEmploye));
+		return Response.status(200).entity(gson.toJson(cadreservices.compteurDeConges(idConge, idEmploye))).build();
+	}
+	
+	@GET
+	@Path("/listCongesById/{id}")
+	public Response listCongesById(@PathParam("id") String id){
+		CadreServices cadreServices = CadreServices.getInstance();
+		return Response.status(200).entity(gson.toJson(cadreServices.getDemandesDeCongeByIdEmploye(id))).build();
+	}
+	
+	@GET
+	@Path("/listCongesByType/{type}")
+	public Response listCongesByType(@PathParam("type") String type){
+		CadreServices cadreServices = CadreServices.getInstance();
+		return Response.status(200).entity(gson.toJson(cadreServices.getDemandesDeCongeByType(type))).build();
+	}
+		
 	@POST
 	@Path("")
-	public Response addPokemon(){
+	public Response addConge(@FormParam("idConge") int idConge, @FormParam("idEmploye") String idEmploye, @FormParam("dateDebut") String dateDebut, @FormParam("dateFin") String dateFin){
+		CadreServices cadreServices = CadreServices.getInstance();
 		try {
+			cadreServices.setDemandeDeConge(idConge, idEmploye, dateDebut, dateFin);
 			return Response.status(200).entity(gson.toJson("")).build();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	@DELETE
-	@Path("")
-	public Response deletePokemon(){
-		try {
-			return Response.status(200).entity("").build();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,8 +65,10 @@ public class CongesWS {
 	
 	@PUT
 	@Path("")
-	public Response updatePokemon(){
+	public Response updateStateConge(@FormParam("id") Integer id, @FormParam("nb") Integer nb){
+		CadreServices cadreServices = CadreServices.getInstance();
 		try {
+			cadreServices.setStateConge(id, nb);
 			return Response.status(200).entity(gson.toJson("")).build();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -58,4 +76,6 @@ public class CongesWS {
 		}
 		return null;
 	}
+	
+
 }
