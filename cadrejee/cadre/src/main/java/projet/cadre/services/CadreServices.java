91423778de946958b940b0f0890e0b19aa8d8898
chloepelletier <chloe.pelletier@hei.fr>
2017-04-11 15:09:26 +0200

    Create CadreServices.java

---
+++ cadrejee/cadre/src/main/java/projet/cadre/services/CadreServices.java
@@ -0,0 +1,246 @@
+package projet.cadre.services;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import projet.cadre.dao.AttestationsDao;
+import projet.cadre.dao.CongesDao;
+import projet.cadre.dao.ContactDao;
+import projet.cadre.dao.DevisDao;
+import projet.cadre.dao.ElementsSiteDao;
+import projet.cadre.dao.EmployeDao;
+import projet.cadre.dao.EvenementAutreDao;
+import projet.cadre.dao.RappelDao;
+import projet.cadre.dao.RessourceDao;
+import projet.cadre.dao.ValiditesDao;
+import projet.cadre.dao.VehiculesDao;
+import projet.cadre.model.Contact;
+import projet.cadre.model.DemandesAttestation;
+import projet.cadre.model.DemandesConge;
+import projet.cadre.model.DemandesValidite;
+import projet.cadre.model.DemandesVehicule;
+import projet.cadre.model.Devis;
+import projet.cadre.model.ElementsSite;
+import projet.cadre.model.Employe;
+import projet.cadre.model.EvenementAutre;
+import projet.cadre.model.Rappel;
+import projet.cadre.model.Ressource;
+import projet.cadre.model.Vehicules;
+
+public class CadreServices {
+	
+	private static CadreServices instance;
+
+	public static CadreServices getInstance() {
+		if (instance == null) {
+			instance = new CadreServices();
+		}
+		return instance;
+	}
+	
+	private AttestationsDao attestationsDao = new AttestationsDao();
+	private CongesDao congesDao = new CongesDao();
+	private ContactDao contactDao = new ContactDao();
+	private DevisDao devisDao = new DevisDao();
+	private ElementsSiteDao elementsSiteDao = new ElementsSiteDao();
+	private EmployeDao employeDao = new EmployeDao();
+	private EvenementAutreDao evenementAutreDao= new EvenementAutreDao();
+	private RappelDao rappelDao = new RappelDao();
+	private RessourceDao ressourceDao = new RessourceDao();
+	private ValiditesDao validitesDao = new ValiditesDao();
+	private VehiculesDao vehiculesDao = new VehiculesDao(); 
+	
+	// AttestationsDao 
+	public List<DemandesAttestation> getDemandesAttestation(){
+		return attestationsDao.getDemandesAttestation();
+	}
+
+	public List<DemandesAttestation> getDemandesAttestationeByidEmploye(String id){
+		return attestationsDao.getDemandesAttestationeByidEmploye(id);
+	}
+	
+	public List<DemandesAttestation> getDemandesAttestationByEtat(String etat){
+		return attestationsDao.getDemandesAttestationByEtat(etat);
+	}
+	
+	public void setDemandeAttestation(String idAttestation, String idEmploye, String date){
+		attestationsDao.setDemandeAttestation(idAttestation, idEmploye, date);
+	}
+	public void setStateAttestation (int idDemandeAttestation, int nb){
+		attestationsDao.setState(idDemandeAttestation, nb);
+	}
+	
+	//Conges Dao
+	
+
+	public List<DemandesConge> getDemandesDeConge(){
+		return congesDao.getDemandesDeConge();
+	}
+	
+	public List<DemandesConge> getDemandesDeCongeByIdEmploye(String id){
+		return congesDao.getDemandesDeCongeByIdEmploye(id);
+	}
+	
+	public List<DemandesConge> getDemandesDeCongeByType(String type){
+		return congesDao.getDemandesDeCongeByType(type);
+	}
+	
+	public void setDemandeDeConge(int idConge, String idEmploye, String dateDebut, String dateFin){
+		congesDao.setDemandeDeConge(idConge, idEmploye, dateDebut, dateFin);
+	}
+	
+	public void setStateConge(int id, int nb){
+		congesDao.setState(id, nb);
+	}
+	
+	public int compteurDeConges(int idConge, String idEmploye){
+		return congesDao.compteurDeConges(idConge, idEmploye);
+	}
+	
+	// Contact DAO
+	
+	public Contact saveContact(Contact contact){
+		return contactDao.saveContact(contact);
+	}
+	
+	// Devis DAO 
+	
+	public Devis saveDevis(Devis devis){
+		return devisDao.saveDevis(devis);
+	}
+	
+	//ElementsSite Dao
+	public void modifierElementTexte(String idElement, String contenuElement) {
+		elementsSiteDao.modifierElementTexte(idElement, contenuElement);
+	}
+	
+	public void modifierElementImage(String idElement, String contenuElement, String cheminElement) {
+		elementsSiteDao.modifierElementImage(idElement, contenuElement, cheminElement);
+	}
+	
+	public ElementsSite getElementById(String id) {
+		return elementsSiteDao.getElementById(id) ;
+	}
+
+	//Employe DAO 
+	
+	public HashMap<String,String> hashIdMdp(){
+		return employeDao.hashIdMdp();
+	}
+	
+	public Employe getEmployeById(String idEmploye) {
+		return employeDao.getEmployeById(idEmploye);
+	}
+	
+	public List<Employe> listEmployes() {
+		return employeDao.listEmployes();
+	}
+	
+	public Employe saveEmploye(Employe employe) {
+		return employeDao.saveEmploye(employe);
+	}
+	
+	public void deleteEmploye(Employe employe){
+		employeDao.deleteEmploye(employe);
+	}
+	
+	public List<Employe> listEmployesPosteNom(String champs) {
+		return employeDao.listEmployesPosteNom(champs);
+	}
+		
+	public void modifierMDP(String idEmploye, String motDePasse) {
+		employeDao.modifierMDP(idEmploye, motDePasse);	
+	}
+
+	// Evenement Autre Dao 
+	
+	public EvenementAutre saveEvenementAutre(EvenementAutre event) {
+		return evenementAutreDao.saveEvenementAutre(event);
+	}
+
+	public void deleteevenement(EvenementAutre evenement){
+		evenementAutreDao.deleteevenement(evenement);
+	}
+
+	public EvenementAutre getevenementById(int idevenement ) {
+		return evenementAutreDao.getevenementById(idevenement);
+	}
+
+	//Rappel Dao
+	public Rappel saveRappel(Rappel rappel) {
+		return rappelDao.saveRappel(rappel);
+	}
+	
+	public void deleteRappel(Rappel rappel){
+		rappelDao.deleteRappel(rappel);
+	}
+	
+	public Rappel getRappelById(int idRappel ) {
+		return rappelDao.getRappelById(idRappel);
+	}
+	
+	//Ressource Dao
+	public  Ressource getRessourceById(int id) {
+		return ressourceDao.getRessourceById(id);
+	}
+	
+	public List<Ressource> listRessourceByPosteNom(String champs) {
+		return ressourceDao.listRessourceByPosteNom(champs);
+	}
+		
+	public Ressource saveRessource(Ressource ressource) {
+		return ressourceDao.saveRessource(ressource);
+	}
+	
+	public void deleteRessource(int id){
+		ressourceDao.deleteRessource(id);
+	}
+	
+	//Validites Dao
+	public List<DemandesValidite> getDemandesValidite(){
+		return validitesDao.getDemandesValidite();
+	}
+	
+	public List<DemandesValidite> getDemandesDeValiditeByidEmploye(String id){
+		return validitesDao.getDemandesDeValiditeByidEmploye(id);
+	}
+	
+	public void setDemandeDeValidite(int idValidite, String idEmploye, Date dateDebut, Date dateFin){
+		validitesDao.setDemandeDeValidite(idValidite, idEmploye, dateDebut, dateFin);
+	}
+	
+	public void setStateValidite (int idValidite, String idEmploye, int nb){
+		validitesDao.setState(idValidite, idEmploye, nb);
+	}
+	
+	//Vehicules Dao
+	public Vehicules addVehicule(String immatriculation, String typeVehicule){
+		return vehiculesDao.addVehicule(immatriculation, typeVehicule);
+	}
+	
+	public void deleteVehicule(String immatriculation){
+		vehiculesDao.deleteVehicule(immatriculation);
+	}
+
+	public List<DemandesVehicule> getDemandesDeVehicule(){
+		return vehiculesDao.getDemandesDeVehicule();
+	}
+	
+	public List<DemandesVehicule> getDemandesDeVehiculeByIdEmploye(String id){
+		return vehiculesDao.getDemandesDeVehiculeByIdEmploye(id);
+	}
+	
+	public List<DemandesAttestation> getDemandesVehiculeByEtat(String etat){
+		return vehiculesDao.getDemandesVehiculeByEtat(etat);
+	}
+	
+	public void setDemandeDeVehicule (String immatriculation, String idEmploye, Date dateDebut, Date dateFin){
+		vehiculesDao.setDemandeDeVehicule(immatriculation, idEmploye, dateDebut, dateFin);
+	}
+	
+	public void setStateVehicule(String immatriculation, String idEmploye, int nb){
+		vehiculesDao.setState(immatriculation, idEmploye, nb);
+	}
+
+}
