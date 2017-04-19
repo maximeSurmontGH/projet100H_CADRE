8106c02603b43e2e4af3f9883701412e231b5134
chloepelletier <chloe.pelletier@hei.fr>
2017-04-20 00:15:38 +0200

    lien bdd site vitrine debut

---
+++ cadrejee/cadre/src/main/webapp/js/entreprise.js
@@ -0,0 +1,76 @@
+function getSlogan(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/aslogan",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var pslo = document.getElementById("slogan");
+		var newh=document.createElement("h5");
+		newh.textContent=this.response.contenuElement;
+		pslo.appendChild(newh);
+	}
+	getList2.send();
+}
+
+function getHisto1(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/histo1",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var phisto = document.getElementById("histo1");
+		var newp=document.createElement("p");
+		newp.textContent=this.response.contenuElement;
+		phisto.appendChild(newp);
+	}
+	getList2.send();
+}
+
+function getHisto2(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/histo2",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var phisto = document.getElementById("histo2");
+		var newp=document.createElement("p");
+		newp.textContent=this.response.contenuElement;
+		phisto.appendChild(newp);
+	}
+	getList2.send();
+}
+
+function getHisto3(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/histo3",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var phisto = document.getElementById("histo3");
+		var newp=document.createElement("p");
+		newp.textContent=this.response.contenuElement;
+		phisto.appendChild(newp);
+	}
+	getList2.send();
+}
+
+function getHisto4(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/histo4",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var phisto = document.getElementById("histo4");
+		var newp=document.createElement("p");
+		newp.textContent=this.response.contenuElement;
+		phisto.appendChild(newp);
+	}
+	getList2.send();
+}
+
+
+
+
+
+window.onload = function(){
+	getSlogan();
+	getHisto1();
+	getHisto2();
+	getHisto4();
+	getHisto3();
+};
---
+++ cadrejee/cadre/src/main/webapp/js/index.js
@@ -0,0 +1,35 @@
+
+
+function getDescription(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/ades",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var pdes = document.getElementById("description");
+		var newp=document.createElement("p");
+		newp.textContent=this.response.contenuElement;
+		pdes.appendChild(newp);
+	}
+	getList2.send();
+}
+
+
+function getSlogan(){
+	var getList2 = new XMLHttpRequest();
+	getList2.open("GET","cadrews/elementssite/getElementById/aslogan",true, null, null);
+	getList2.responseType="json";
+	getList2.onload=function(){
+		var pslo = document.getElementById("slogan");
+		var newh=document.createElement("h5");
+		newh.textContent=this.response.contenuElement;
+		pslo.appendChild(newh);
+	}
+	getList2.send();
+}
+
+
+
+window.onload = function(){
+	getDescription();
+	getSlogan();
+};
