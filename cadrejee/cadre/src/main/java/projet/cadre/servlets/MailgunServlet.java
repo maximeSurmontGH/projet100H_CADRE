73e68451c1dcd020365474994d2a332a5e4f1db9
maxime SURMONT <maxime.surmont@hei.yncrea.fr>
2017-05-01 20:36:49 +0200

    suppression login/mdp mailgun

--- cadrejee/cadre/src/main/java/projet/cadre/servlets/MailgunServlet.java
+++ cadrejee/cadre/src/main/java/projet/cadre/servlets/MailgunServlet.java
@@ -17,8 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 
-// maxime.surmont@hei.fr
-// kegkmex9
 @SuppressWarnings("serial")
 @WebServlet(name = "mailgun", value = "/contact/send/email")
 public class MailgunServlet extends HttpServlet {
