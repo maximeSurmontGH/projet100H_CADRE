11138b84db9bd1b4a2a722188dbab24dfe7b11ac
chloepelletier <chloe.pelletier@hei.fr>
2017-05-04 09:11:42 +0200

    remise a la version chloe

--- cadrejee/cadre/src/main/java/projet/cadre/dao/DataSourceProvider.java
+++ cadrejee/cadre/src/main/java/projet/cadre/dao/DataSourceProvider.java
@@ -10,11 +10,11 @@ public class DataSourceProvider {
 	public static DataSource getDataSource() {
 		if (dataSource == null) {
 			dataSource = new MysqlDataSource();
-			dataSource.setServerName("localhost");
+			dataSource.setServerName("vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
 			dataSource.setPort(3306);
-			dataSource.setDatabaseName("cadre");
-			dataSource.setUser("root");
-			dataSource.setPassword("1994");
+			dataSource.setDatabaseName("ohwx147hd9g2vgz4");
+			dataSource.setUser("ifw3s4zebh0lo8js");
+			dataSource.setPassword("bkc7x46u5gzdykhs");
 		}
 		return dataSource;
 	}
